f4aec0f647bb17f2d28d6c2c81220c36577bc0a3
Andrew <almatinez101@yandex.ru>
2020-08-19 01:11:25 +0700

    keyboard start

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from request import get_lyrics, get_urls
+from keyboards import prepare_keyboard
 
 
 API_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
@@ -22,7 +23,7 @@ async def send_welcome(message: types.Message):
     This handler will be called when user sends `/start` or `/help` command
     """
     await message.answer("Hi!\nI'm RapBot!\nWhich song lyrics do you want to see?\
-    \nExample: Boulevard Depo - OFMD")
+    \nExample: Boulevard Depo OFMD")
 
 
 @dp.message_handler()
@@ -30,14 +31,21 @@ async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
     
-    try:
-        urls = get_urls(message.text)
-        await message.answer(get_lyrics(urls[0]))
-        await message.answer_photo(urls[1])
-        print('OK:', message.text)
-    except:
-        await message.answer("Sorry, I can't find that song(")
-        print('FAIL:', message.text)
+    #try:
+    d = {
+        1 : 'a',
+        2 : 'b', 
+        3 : 'c'
+    }
+    keybkoard = prepare_keyboard(d)
+    urls = get_urls(message.text)
+    await message.answer(get_lyrics(urls[0]), reply_markup=keybkoard)
+    await message.answer_photo(urls[1])
+    print('OK:', message.text)
+
+    # except:
+    #     await message.answer("Sorry, I can't find that song(")
+    #     print('FAIL:', message.text)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
